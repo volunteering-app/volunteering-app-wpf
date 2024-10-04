8bed6be34c7425d78fd212644d84da34111f888c
REFLAXua <114022873+REFLAXua@users.noreply.github.com>
2024-10-04 09:23:33 +0300

    test massage

--- FRONT/FRONT/App.xaml.cs
+++ FRONT/FRONT/App.xaml.cs
@@ -1,15 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows;
+﻿// <copyright file="App.xaml.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
 
 namespace FRONT
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Configuration;
+    using System.Data;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using System.Windows;
+
     /// <summary>
-    /// Логика взаимодействия для App.xaml
+    /// Логика взаимодействия для App.xaml.
     /// </summary>
     public partial class App : Application
     {
--- FRONT/FRONT/Properties/AssemblyInfo.cs
+++ FRONT/FRONT/Properties/AssemblyInfo.cs
@@ -1,4 +1,8 @@
-﻿using System.Reflection;
+﻿// <copyright file="AssemblyInfo.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+using System.Reflection;
 using System.Resources;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
@@ -21,25 +25,25 @@ using System.Windows;
 // из модели COM, установите атрибут ComVisible для этого типа в значение true.
 [assembly: ComVisible(false)]
 
-//Чтобы начать создание локализуемых приложений, задайте
-//<UICulture>CultureYouAreCodingWith</UICulture> в файле .csproj
-//в <PropertyGroup>. Например, при использовании английского (США)
-//в своих исходных файлах установите <UICulture> в en-US.  Затем отмените преобразование в комментарий
-//атрибута NeutralResourceLanguage ниже.  Обновите "en-US" в
-//строка внизу для обеспечения соответствия настройки UICulture в файле проекта.
+// Чтобы начать создание локализуемых приложений, задайте
+// <UICulture>CultureYouAreCodingWith</UICulture> в файле .csproj
+// в <PropertyGroup>. Например, при использовании английского (США)
+// в своих исходных файлах установите <UICulture> в en-US.  Затем отмените преобразование в комментарий
+// атрибута NeutralResourceLanguage ниже.  Обновите "en-US" в
+// строка внизу для обеспечения соответствия настройки UICulture в файле проекта.
 
-//[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
-
+// [assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
 
 [assembly: ThemeInfo(
-    ResourceDictionaryLocation.None, //где расположены словари ресурсов по конкретным тематикам
-                                     //(используется, если ресурс не найден на странице,
-                                     // или в словарях ресурсов приложения)
-    ResourceDictionaryLocation.SourceAssembly //где расположен словарь универсальных ресурсов
-                                              //(используется, если ресурс не найден на странице,
-                                              // в приложении или в каких-либо словарях ресурсов для конкретной темы)
-)]
+    ResourceDictionaryLocation.None, // где расположены словари ресурсов по конкретным тематикам
 
+                                     // (используется, если ресурс не найден на странице,
+                                     // или в словарях ресурсов приложения)
+    ResourceDictionaryLocation.SourceAssembly) // где расположен словарь универсальных ресурсов
+
+                                              // (используется, если ресурс не найден на странице,
+                                              // в приложении или в каких-либо словарях ресурсов для конкретной темы)
+]
 
 // Сведения о версии для сборки включают четыре следующих значения:
 //
@@ -48,7 +52,7 @@ using System.Windows;
 //      Номер сборки
 //      Номер редакции
 //
-// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// Можно задать все значения или принять номера сборки и редакции по умолчанию
 // используя "*", как показано ниже:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
